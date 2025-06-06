f76b3206842ac0d3d8d8b368c298e22289018b61
Vinigavaa <vinigava14@gmail.com>
2025-06-06 20:52:12 -0300

    refactor: adicionando anotação de repositorio

--- src/main/java/com/eventos/tec/repositories/EnderecoRepository.java
+++ src/main/java/com/eventos/tec/repositories/EnderecoRepository.java
@@ -2,8 +2,10 @@ package com.eventos.tec.repositories;
 
 import com.eventos.tec.domain.endereco.Endereco;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface EnderecoRepository extends JpaRepository<Endereco, UUID> {
 }
--- src/main/java/com/eventos/tec/repositories/EventRepository.java
+++ src/main/java/com/eventos/tec/repositories/EventRepository.java
@@ -2,8 +2,10 @@ package com.eventos.tec.repositories;
 
 import com.eventos.tec.domain.event.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface EventRepository extends JpaRepository<Event, UUID> {
 }
